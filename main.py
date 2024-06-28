607af00345f01d52a5120738c5f679d5b735e687
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-28 12:19:52 +0500

    my commit

--- main.py
+++ main.py
@@ -19,4 +19,14 @@ def main():
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                
+                run = False
+                break
+
+
+    
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
+
