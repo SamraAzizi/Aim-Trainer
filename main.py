b311d04079e7c1f19e72705a770c58b87f7471cd
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-28 12:18:48 +0500

    my commit

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ pygame.init()
 WIDTH, HEIGHT = 800,600
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Aim Trainer")
+
+
+def main():
+    run = True
+
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                
