1c56d2feac6f67279f059e5da242ea1fc057b1e7
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-29 17:43:04 +0500

    my commit

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ WIDTH, HEIGHT = 800,600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Trainer")
 
+TARGET_INCREMENT = 400
+TARGET_EVENT = pygame.USEREVENT
+
 class Target:
     MAX_SIZE = 30
     GROWTH_RATE = 0.2
     COLOR = "red"
+    SECOND_COLOR = "white"
 
     def __init__(self, x, y):
         self.x = x
@@ -33,12 +37,16 @@ class Target:
 
 
     def draw(self, win):
-        pygame.draw.circle()
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size)
+        pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
+        pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
 
 
 def main():
     run = True
 
+    targets = []
 
     while run:
         for event in pygame.event.get():
