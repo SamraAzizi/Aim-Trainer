b991540e85e6f7d5f1d27cc87668fc52e1b55487
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-01 11:21:21 +0500

    my commit

--- main.py
+++ main.py
@@ -58,6 +58,29 @@ def draw(win, target):
 def main():
     run = True
 
+    targets = []
+    clock = pygame.time.Clock()
+    target_pressed = 0
+    
+    clicks = 0
+    misses = 0
+    start_time = time.time()
+    pygame.time.set_timer(TARGET_EVENT, TARGET_INCREMENT)
+
+    while run:
+        clock.tick(60)
+        click = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+            if event.type == TARGET_EVENT:
+                x = random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING )
+                y = random.randint(TARGET_PADDING, HEIGHT - TARGET_PADDING)
+                target = Target(x, y)
+                targets.append(target)
+
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 click = True
