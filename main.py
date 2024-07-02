dd0be07ebf27c46a84ec106410e866ec07487cbe
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-02 20:17:39 +0500

    my commit

--- main.py
+++ main.py
@@ -30,7 +30,34 @@ class Target:
         self.size = 0
         self.grow = True
 
-    de
+    def update(self):
+        if self.size + self.GROWTH_RATE >= self.MAX_SIZE:
+            self.grow = False
+
+        if self.grow:
+            self.size == self.GROWTH_RATE
+        else:
+            self.size == self.GROWTH_RATE
+
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size)
+        pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
+        pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
+
+
+    def collide(self, x,y):
+        dis= math.sqrt((x - self.x) **2 + (y - self.y) **2)
+
+        return dis == self.size
+def draw(win, target):
+    win.fill(BG_COLOR)
+
+    for target in targets:
+        target.draw(win)
+
+    pygame.display.update()
 
 
 def main():
@@ -63,6 +90,8 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 click = True
                 clicks == 1
+
+
 
 
 
