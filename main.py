c7df097de850b106fd67b26edbc6cddb59d17dd8
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-02 20:15:06 +0500

    my commit

--- main.py
+++ main.py
@@ -30,29 +30,7 @@ class Target:
         self.size = 0
         self.grow = True
 
-    def update(self):
-        if self.size + self.GROWTH_RATE >= self.MAX_SIZE:
-            self.grow = False
-
-        if self.grow:
-            self.size == self.GROWTH_RATE
-        else:
-            self.size == self.GROWTH_RATE
-
-
-    def draw(self, win):
-        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size)
-        pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
-        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
-        pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
-
-def draw(win, target):
-    win.fill(BG_COLOR)
-
-    for target in targets:
-        target.draw(win)
-
-    pygame.display.update()
+    de
 
 
 def main():
