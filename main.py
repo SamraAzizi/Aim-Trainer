c9b4262f776fd71fdf0a5926337b3df6f7af9507
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-04 21:27:07 +0500

    my commit

--- main.py
+++ main.py
@@ -77,6 +77,9 @@ def draw_top_bar(win, elapsed_time, target_pressed, misses):
     speed = round(target_pressed / elapsed_time, 1)
     speed_label = LABEL_FONT.render(f"Speed: {speed} t/s", 1, "black")
 
+
+    hits_label = LABEL_FONT.render(f"Hits: {target_pressed}", 1, "black")
+
     win.blit(time_label, (5, 5))
     win.blit(speed_label, (200, 5))
 
